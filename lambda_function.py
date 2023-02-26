b8895c58e6645ffb8037ae34efb0f19854ad5957
kleber.barilli <kleber.barilli73@gmail.com>
2023-02-26 19:28:05 -0300

    teste, salvando csv no S3

--- lambda_function.py
+++ lambda_function.py
@@ -2,8 +2,18 @@ import json
 import urllib.request
 from io import StringIO
 import pandas as pd
-
+import boto3
 def main():
+    aws_access_key_id = 'access key'
+    aws_secret_access_key = 'secret key'
+    aws_region = 'region'
+
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name=aws_region
+    )
 
     res = urllib.request.urlopen(urllib.request.Request(
         url='https://swapi.dev/api/people/',
@@ -13,6 +23,15 @@ def main():
 
     df = pd.DataFrame(res_json['results'])
 
-    df.to_csv('df.csv', index=False)
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer, index=False)
+
+
+    bucket_name = 'bucket-name'
+    s3.put_object(
+        Bucket=bucket_name,
+        Key='files/df.csv',
+        Body=csv_buffer.getvalue()
+    )
 
 main()
